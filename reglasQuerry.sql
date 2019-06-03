5d71d51514250a406cd5dda57a2fbe1ca3cf4c96
lulamasini <51343208+lulamasini@users.noreply.github.com>
2019-06-03 19:01:54 -0300

    Update reglasQuerry.sql

--- reglasQuerry.sql
+++ reglasQuerry.sql
@@ -1,17 +1,28 @@
 --Regla sigla
+ALTER TABLE Planes 
+    ADD CONSTRAINT regla_Planes_sigla CHECK( Sigla NOT like '%[0-9]%');
+    
 --Regla dni
+ALTER TABLE Pacientes
+    ADD CONSTRAINT regla_Pacientes_dni CHECK(isnumeric(DNI));
 
 --Regla Categoria
 ALTER TABLE ObraSocial
     ADD CONSTRAINT regla_ObraSocial_categoria CHECK (Categoria IN ('os', 'pp'));
+                                                                  
 --Regla sexo
 ALTER TABLE Pacientes
-    ADD CONSTRAINT regla_pacientes_sexo CHECK (Sexo IN ('f','m'));
+    ADD CONSTRAINT regla_Pacientes_sexo CHECK (Sexo IN ('f','m'));
 ALTER TABLE Medicos
-    ADD CONSTRAINT regla_pacientes_sexo CHECK (Sexo IN ('f','m'));
+    ADD CONSTRAINT regla_Pacientes_sexo CHECK (Sexo IN ('f','m'));
 
---Regla fecha de estudio
---Regla fecha de nacimiento
+--Regla fecha de estudio(mañana le voy a preguntar bien al profesor)
+ALTER TABLE Registros 
+    ADD CONSTRAINT regla_Registros_fechaEstudio CHECK ( datediff(dd,Fecha,getdate())>1 AND abs(datediff(dd,Fecha,getdate())-30)<31);                                                         
+                                                        
+--Regla fecha de nacimiento("")
+ALTER TABLE Pacientes 
+    ADD CONSTRAINT regla_pacientes_Nacimiento CHECK (20<cast(datediff(dd, Nacimiento ,GETDATE()) / 365.25 as int)< 80);                                                                 
 
 --Regla NroPlan
 ALTER TABLE Planes
